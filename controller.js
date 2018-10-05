3cb3668fa0b032b30f7e5ebc2222767d46eb8c7a
Unknown <zolt0077@edu.eal.dk>
2018-10-05 11:55:50 +0100

    Quick refactor

--- controller.js
+++ controller.js
@@ -5,19 +5,13 @@ exports.stairs = function (req, res) {
   let stairPos = 0;
   let flightPos = 0;
   let countStrides = 0;
+  let lastFlight = flights.length - 1;
 
-  let reached = false;
-  while (!reached) {
-    // Was it the last flight?
-    if (flights.length == flightPos) {
-      reached = true
-      break
-    }
-    console.log(stairPos, flights[flightPos])
+  while (lastFlight >= flightPos) {
     // Have we reached the end of this flight?
     if (stairPos >= flights[flightPos]) {
-      // Make a turn, always 2
-      if (flights.length-1  != flightPos) countStrides += 2;
+      // Make a turn - always 2 (skip this for the last flight)
+      if (lastFlight != flightPos) countStrides += 2;
       // On to the next flight
       ++flightPos;
       stairPos = 0;
